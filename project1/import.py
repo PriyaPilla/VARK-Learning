8dfb4ac6489d4df358a18c87a796f8dc2f3c2516
PriyaPilla <hp22@students.uwf.edu>
2022-03-07 19:00:46 -0600

    debugging

--- project1/import.py
+++ project1/import.py
@@ -7,7 +7,8 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine(os.getenv("DATABASE_URL"))
+#engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("DATABASE_URL"), pool_pre_ping=True)
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
