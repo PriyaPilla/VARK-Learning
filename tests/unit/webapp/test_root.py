797665a5582e530144ef5697bae5f7572d73c8e3
ah181 <59804478+ah181@users.noreply.github.com>
2022-04-19 00:04:32 -0500

    Delete tests directory
    moved to project1/tests

--- tests/unit/webapp/test_root.py
+++
@@ -1,9 +0,0 @@
-from tests.unit.webapp import client
-
-def test_landing(client):
-    #client paramter filled with fixture in conftest.py
-    landing = client.get("/")
-    html = landing.data.decode()
-    # Spot check important text
-    assert "WELCOME TO VARK LEARNING!" in html
-    assert landing.status_code == 200
