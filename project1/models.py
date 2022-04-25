890ac98cbee1f6d24052918fa8f0a20e42dbbc28
PriyaPilla <59668100+PriyaPilla@users.noreply.github.com>
2022-04-24 21:34:43 -0500

    deleted extra table

--- project1/models.py
+++ project1/models.py
@@ -1,17 +1,8 @@
 #classes for each table 
-#delete tables before using
 
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-# class Book(db.Model):
-#     __tablename__ = "books"
-#     id = db.Column(db.Integer, primary_key =True)
-#     isbn = db.Column(db.String, nullable=False)
-#     title = db.Column(db.String, nullable=False)
-#     author = db.Column(db.String, nullable=False)
-#     publicationyear = db.Column(db.Integer, nullable=False)
 
 class Student(db.Model):
     __tablename__ = "student"
